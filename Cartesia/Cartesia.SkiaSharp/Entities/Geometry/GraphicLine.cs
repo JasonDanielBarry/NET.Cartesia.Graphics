72e131745b9a3a29b55a3c6e6871883d0394f120
JasonBarry <jasondanielbarry512@gmail.com>
2026-09-02 22:29:36 +0200

    feat(entities): create geometry entities

---
+++ Cartesia/Cartesia.SkiaSharp/Entities/Geometry/GraphicLine.cs
@@ -0,0 +1,12 @@
+﻿using Cartesia.Core.Geometry;
+using Cartesia.SkiaSharp.Entities.Base;
+using Cartesia.SkiaSharp.Rendering;
+
+namespace Cartesia.SkiaSharp.Entities.Geometry
+{
+	public sealed class GraphicLine(Point startPointIn, Point endPointIn, Pen strokeIn) : GraphicEntity(Brush.None, strokeIn)
+	{
+		public Point StartPoint => startPointIn;
+		public Point EndPoint => endPointIn;
+	}
+}
---
+++ Cartesia/Cartesia.SkiaSharp/Entities/Geometry/GraphicPolygon.cs
@@ -0,0 +1,13 @@
+﻿using Cartesia.Core.Geometry;
+using Cartesia.SkiaSharp.Entities.Base;
+using Cartesia.SkiaSharp.Rendering;
+
+namespace Cartesia.SkiaSharp.Entities.Geometry
+{
+	public sealed class GraphicPolygon(IReadOnlyList<Point> verticesIn, Brush fillIn, Pen strokeIn) : GraphicEntity(fillIn, strokeIn)
+	{
+		private readonly Point[] _vertices = verticesIn.ToArray();
+
+		public Point[] Vertices => _vertices;
+	}
+}
