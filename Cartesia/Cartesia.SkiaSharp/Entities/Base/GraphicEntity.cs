04817eefb187ffdbf8a5bb9c2ff86994100dfb1f
JasonBarry <jasondanielbarry512@gmail.com>
2026-09-02 22:28:53 +0200

    feat: set up Cartesia.SkiaSharp

---
+++ Cartesia/Cartesia.SkiaSharp/Entities/Base/GraphicEntity.cs
@@ -0,0 +1,21 @@
+﻿using Cartesia.SkiaSharp.Rendering;
+
+namespace Cartesia.SkiaSharp.Entities.Base
+{
+	public abstract class GraphicEntity
+	{
+		private protected readonly Brush _fill;
+		private protected readonly Pen _stroke;
+
+		private protected GraphicEntity(Brush fillIn, Pen strokeIn)
+		{
+			_fill = fillIn;
+			_stroke = strokeIn;
+		}
+
+		internal abstract void Draw();
+
+		public Brush Fill => _fill;
+		public Pen Stroke => _stroke;
+	}
+}
---
+++ Cartesia/Cartesia.SkiaSharp/Rendering/Brush.cs
@@ -0,0 +1,11 @@
+﻿using SkiaSharp;
+
+namespace Cartesia.SkiaSharp.Rendering
+{
+	public sealed class Brush(SKColor colourIn)
+	{
+		public SKColor Colour => colourIn;
+
+		public static Brush None => new Brush(SKColors.Transparent);
+	}
+}
