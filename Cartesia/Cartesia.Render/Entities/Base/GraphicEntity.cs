d2b0bd4576ab4cd3eb2d5e2227815516f3ece397
JasonBarry <jasondanielbarry512@gmail.com>
2026-09-06 14:34:33 +0200

    feat(entity): create GraphicEntity base class

---
+++ Cartesia/Cartesia.Render/Entities/Base/GraphicEntity.cs
@@ -0,0 +1,38 @@
+﻿using Cartesia.Core.Geometry;
+using Cartesia.Core.Mapping;
+using Cartesia.Render.Rendering;
+using SkiaSharp;
+
+namespace Cartesia.Render.Entities.Base
+{
+	public abstract class GraphicEntity
+	{
+		private protected readonly Brush _fillBrush;
+		private protected readonly Pen _strokePen;
+
+		private protected GraphicEntity(Brush fillIn, Pen strokeIn)
+		{
+			ArgumentNullException.ThrowIfNull(fillIn, nameof(fillIn));
+			ArgumentNullException.ThrowIfNull(strokeIn, nameof(strokeIn));
+
+			_fillBrush = fillIn;
+			_strokePen = strokeIn;
+
+			_fillPaint = fillIn.ToSKPaint();
+			_strokePaint = strokeIn.ToSKPaint();
+		}
+
+		protected readonly SKPaint
+			_fillPaint,
+			_strokePaint;
+
+		internal abstract Box BoundingBox();
+
+		internal abstract void Draw(SKCanvas canvasIn);
+
+		internal abstract void Precompute(WorldToCanvasMapper mapperIn);
+
+		public Brush Fill => _fillBrush;
+		public Pen Stroke => _strokePen;
+	}
+}
