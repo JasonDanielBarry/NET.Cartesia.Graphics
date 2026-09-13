b9f6cb3da0d1410416425cd0741043d70556d565
JasonBarry <jasondanielbarry512@gmail.com>
2026-09-13 21:53:07 +0200

    test(core): add Box and Alignment suites incl two-corner Transform documentation

---
+++ Cartesia/Cartesia.Tests/Core/AlignmentTests.cs
@@ -0,0 +1,133 @@
+using Cartesia.Core.Geometry;
+using Cartesia.Core.Shapes;
+using Cartesia.Tests.Helpers;
+
+namespace Cartesia.Tests.Core;
+
+public sealed class AlignmentTests
+{
+    private static Box BoxFor(double w, double h, HorizontalAlignment ha, VerticalAlignment va, double hx = 0, double hy = 0) =>
+        Box.FromDimensionsAndHandle(w, h, ha, va, new Point(hx, hy));
+
+    [Theory]
+    [InlineData(HorizontalAlignment.Left, 0, 10)]
+    [InlineData(HorizontalAlignment.Centre, -5, 5)]
+    [InlineData(HorizontalAlignment.Right, -10, 0)]
+    public void HorizontalBounds_AllCases(HorizontalAlignment ha, double expLeft, double expRight)
+    {
+        Box box = BoxFor(10, 6, ha, VerticalAlignment.Bottom);
+
+        TestHelpers.Near(expLeft, box.BottomLeft.X);
+        TestHelpers.Near(expRight, box.TopRight.X);
+    }
+
+    [Theory]
+    [InlineData(VerticalAlignment.Bottom, 0, 6)]
+    [InlineData(VerticalAlignment.Centre, -3, 3)]
+    [InlineData(VerticalAlignment.Top, -6, 0)]
+    public void VerticalBounds_AllCases(VerticalAlignment va, double expBottom, double expTop)
+    {
+        Box box = BoxFor(10, 6, HorizontalAlignment.Left, va);
+
+        TestHelpers.Near(expBottom, box.BottomLeft.Y);
+        TestHelpers.Near(expTop, box.TopRight.Y);
+    }
+
+    [Fact]
+    public void Centre_OddDimensions_HalvesExactly()
+    {
+        Box box = BoxFor(7, 5, HorizontalAlignment.Centre, VerticalAlignment.Centre);
+
+        TestHelpers.Near(-3.5, box.BottomLeft.X);
+        TestHelpers.Near(3.5, box.TopRight.X);
+        TestHelpers.Near(-2.5, box.BottomLeft.Y);
+        TestHelpers.Near(2.5, box.TopRight.Y);
+    }
+
+    [Fact]
+    public void ZeroDimensions_AllAlignments_CollapseToHandle()
+    {
+        foreach (HorizontalAlignment ha in Enum.GetValues<HorizontalAlignment>())
+        {
+            foreach (VerticalAlignment va in Enum.GetValues<VerticalAlignment>())
+            {
+                Box box = BoxFor(0, 0, ha, va, 11, 22);
+
+                TestHelpers.Near(new Point(11, 22), box.BottomLeft);
+                TestHelpers.Near(new Point(11, 22), box.TopRight);
+            }
+        }
+    }
+
+    [Fact]
+    public void HandleOffset_ShiftsAllAlignmentsEqually()
+    {
+        Box atOrigin = BoxFor(10, 6, HorizontalAlignment.Centre, VerticalAlignment.Centre);
+        Box shifted = BoxFor(10, 6, HorizontalAlignment.Centre, VerticalAlignment.Centre, 100, -50);
+
+        TestHelpers.Near(atOrigin.BottomLeft.X + 100, shifted.BottomLeft.X);
+        TestHelpers.Near(atOrigin.BottomLeft.Y - 50, shifted.BottomLeft.Y);
+        TestHelpers.Near(atOrigin.TopRight.X + 100, shifted.TopRight.X);
+        TestHelpers.Near(atOrigin.TopRight.Y - 50, shifted.TopRight.Y);
+    }
+
+    [Fact]
+    public void InvalidHorizontalAlignment_ThrowsArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            Box.FromDimensionsAndHandle(10, 6, (HorizontalAlignment)999, VerticalAlignment.Bottom, new Point(0, 0)));
+    }
+
+    [Fact]
+    public void InvalidVerticalAlignment_ThrowsArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            Box.FromDimensionsAndHandle(10, 6, HorizontalAlignment.Left, (VerticalAlignment)999, new Point(0, 0)));
+    }
+
+    [Fact]
+    public void InvalidHorizontalAlignment_ParamNameIsAlignment()
+    {
+        ArgumentException ex = Assert.Throws<ArgumentException>(() =>
+            Alignment.HorizontalAndVerticalBounds(10, 6, (HorizontalAlignment)(-1), VerticalAlignment.Bottom));
+
+        Assert.Equal("horizontalAlignmentIn", ex.ParamName);
+    }
+
+    [Fact]
+    public void InvalidVerticalAlignment_ParamNameIsAlignment()
+    {
+        ArgumentException ex = Assert.Throws<ArgumentException>(() =>
+            Alignment.HorizontalAndVerticalBounds(10, 6, HorizontalAlignment.Left, (VerticalAlignment)(-1)));
+
+        Assert.Equal("verticalAlignmentIn", ex.ParamName);
+    }
+
+    [Fact]
+    public void HorizontalAndVerticalBounds_Direct_AllNineCombos()
+    {
+        (HorizontalAlignment h, VerticalAlignment v, double l, double r, double b, double t)[] cases =
+        [
+            (HorizontalAlignment.Left, VerticalAlignment.Bottom, 0, 4, 0, 2),
+            (HorizontalAlignment.Centre, VerticalAlignment.Bottom, -2, 2, 0, 2),
+            (HorizontalAlignment.Right, VerticalAlignment.Bottom, -4, 0, 0, 2),
+            (HorizontalAlignment.Left, VerticalAlignment.Centre, 0, 4, -1, 1),
+            (HorizontalAlignment.Centre, VerticalAlignment.Centre, -2, 2, -1, 1),
+            (HorizontalAlignment.Right, VerticalAlignment.Centre, -4, 0, -1, 1),
+            (HorizontalAlignment.Left, VerticalAlignment.Top, 0, 4, -2, 0),
+            (HorizontalAlignment.Centre, VerticalAlignment.Top, -2, 2, -2, 0),
+            (HorizontalAlignment.Right, VerticalAlignment.Top, -4, 0, -2, 0),
+        ];
+
+        foreach (var (h, v, l, r, b, t) in cases)
+        {
+            (double left, double right, double bottom, double top) =
+                Alignment.HorizontalAndVerticalBounds(4, 2, h, v);
+
+            Assert.Equal(l, left);
+            Assert.Equal(r, right);
+            Assert.Equal(b, bottom);
+            Assert.Equal(t, top);
+        }
+    }
+}
---
+++ Cartesia/Cartesia.Tests/Core/BoxTests.cs
@@ -0,0 +1,282 @@
+using Cartesia.Core.Geometry;
+using Cartesia.Core.Math;
+using Cartesia.Core.Shapes;
+using Cartesia.Tests.Helpers;
+
+namespace Cartesia.Tests.Core;
+
+public sealed class BoxTests
+{
+    [Fact]
+    public void Ctor_TwoPoints_SetsCornersAndDimensions()
+    {
+        Box box = new([new Point(1, 2), new Point(4, 6)]);
+
+        TestHelpers.Near(new Point(1, 2), box.BottomLeft);
+        TestHelpers.Near(new Point(4, 6), box.TopRight);
+        TestHelpers.Near(3, box.Width);
+        TestHelpers.Near(4, box.Height);
+    }
+
+    [Fact]
+    public void Ctor_UnorderedPoints_NormalizesMinMax()
+    {
+        Box box = new([new Point(4, 6), new Point(1, 2)]);
+
+        TestHelpers.Near(new Point(1, 2), box.BottomLeft);
+        TestHelpers.Near(new Point(4, 6), box.TopRight);
+    }
+
+    [Fact]
+    public void Ctor_ManyPoints_TakesExtremes()
+    {
+        Box box = new([new Point(0, 0), new Point(10, 1), new Point(3, 9), new Point(-2, 4)]);
+
+        TestHelpers.Near(new Point(-2, 0), box.BottomLeft);
+        TestHelpers.Near(new Point(10, 9), box.TopRight);
+        TestHelpers.Near(12, box.Width);
+        TestHelpers.Near(9, box.Height);
+    }
+
+    [Fact]
+    public void Ctor_SinglePoint_ZeroSizeBox()
+    {
+        Box box = new([new Point(5, -5)]);
+
+        TestHelpers.Near(new Point(5, -5), box.BottomLeft);
+        TestHelpers.Near(new Point(5, -5), box.TopRight);
+        TestHelpers.Near(0, box.Width);
+        TestHelpers.Near(0, box.Height);
+    }
+
+    [Fact]
+    public void Ctor_NegativeCoordinates_Works()
+    {
+        Box box = new([new Point(-10, -20), new Point(-1, -2)]);
+
+        TestHelpers.Near(new Point(-10, -20), box.BottomLeft);
+        TestHelpers.Near(new Point(-1, -2), box.TopRight);
+        TestHelpers.Near(9, box.Width);
+        TestHelpers.Near(18, box.Height);
+    }
+
+    [Fact]
+    public void Ctor_DuplicatePoints_IgnoresDuplicates()
+    {
+        Box box = new([new Point(2, 2), new Point(2, 2), new Point(8, 8), new Point(8, 8)]);
+
+        TestHelpers.Near(6, box.Width);
+        TestHelpers.Near(6, box.Height);
+    }
+
+    [Fact]
+    public void Ctor_EmptyList_ProducesInvertedSentinelBox()
+    {
+        // No points: minima stay at +1e9, maxima at -1e9. Documents degenerate input.
+        Box box = new([]);
+
+        TestHelpers.Near(new Point(1e9, 1e9), box.BottomLeft);
+        TestHelpers.Near(new Point(-1e9, -1e9), box.TopRight);
+        TestHelpers.Near(-2e9, box.Width);
+        TestHelpers.Near(-2e9, box.Height);
+    }
+
+    [Fact]
+    public void FromBoxes_TwoBoxes_SpansBoth()
+    {
+        Box a = new([new Point(0, 0), new Point(2, 2)]);
+        Box b = new([new Point(5, 1), new Point(7, 9)]);
+
+        Box merged = Box.FromBoxes([a, b]);
+
+        TestHelpers.Near(new Point(0, 0), merged.BottomLeft);
+        TestHelpers.Near(new Point(7, 9), merged.TopRight);
+    }
+
+    [Fact]
+    public void FromBoxes_OverlappingBoxes_SpansExtremes()
+    {
+        Box a = new([new Point(0, 0), new Point(10, 10)]);
+        Box b = new([new Point(5, 5), new Point(6, 6)]);
+
+        Box merged = Box.FromBoxes([a, b]);
+
+        TestHelpers.Near(new Point(0, 0), merged.BottomLeft);
+        TestHelpers.Near(new Point(10, 10), merged.TopRight);
+    }
+
+    [Fact]
+    public void FromBoxes_SingleBox_RoundTrips()
+    {
+        Box a = new([new Point(1, 2), new Point(3, 4)]);
+
+        Box merged = Box.FromBoxes([a]);
+
+        TestHelpers.Near(a.BottomLeft, merged.BottomLeft);
+        TestHelpers.Near(a.TopRight, merged.TopRight);
+    }
+
+    [Fact]
+    public void FromBoxes_ThreeBoxesDisjoint_SpansAll()
+    {
+        Box a = new([new Point(-5, -5), new Point(-1, -1)]);
+        Box b = new([new Point(0, 0), new Point(1, 1)]);
+        Box c = new([new Point(10, -3), new Point(12, 7)]);
+
+        Box merged = Box.FromBoxes([a, b, c]);
+
+        TestHelpers.Near(new Point(-5, -5), merged.BottomLeft);
+        TestHelpers.Near(new Point(12, 7), merged.TopRight);
+        TestHelpers.Near(17, merged.Width);
+        TestHelpers.Near(12, merged.Height);
+    }
+
+    public static TheoryData<HorizontalAlignment, VerticalAlignment, double, double, double, double> HandleCases =>
+        new()
+        {
+            { HorizontalAlignment.Left, VerticalAlignment.Bottom, 0, 0, 10, 8 },
+            { HorizontalAlignment.Centre, VerticalAlignment.Centre, 0, 0, 10, 8 },
+            { HorizontalAlignment.Right, VerticalAlignment.Top, 0, 0, 10, 8 },
+            { HorizontalAlignment.Left, VerticalAlignment.Top, 100, 200, 10, 8 },
+            { HorizontalAlignment.Right, VerticalAlignment.Bottom, -50, -60, 10, 8 },
+        };
+
+    [Theory]
+    [MemberData(nameof(HandleCases))]
+    public void FromDimensionsAndHandle_PreservesDimensions(
+        HorizontalAlignment h, VerticalAlignment v, double hx, double hy, double w, double hgt)
+    {
+        Box box = Box.FromDimensionsAndHandle(w, hgt, h, v, new Point(hx, hy));
+
+        TestHelpers.Near(w, box.Width);
+        TestHelpers.Near(hgt, box.Height);
+    }
+
+    [Fact]
+    public void FromDimensionsAndHandle_LeftBottom_OriginAtHandle()
+    {
+        Box box = Box.FromDimensionsAndHandle(10, 8, HorizontalAlignment.Left, VerticalAlignment.Bottom, new Point(3, 4));
+
+        TestHelpers.Near(new Point(3, 4), box.BottomLeft);
+        TestHelpers.Near(new Point(13, 12), box.TopRight);
+    }
+
+    [Fact]
+    public void FromDimensionsAndHandle_Centre_SymmetricAboutHandle()
+    {
+        Box box = Box.FromDimensionsAndHandle(10, 8, HorizontalAlignment.Centre, VerticalAlignment.Centre, new Point(0, 0));
+
+        TestHelpers.Near(new Point(-5, -4), box.BottomLeft);
+        TestHelpers.Near(new Point(5, 4), box.TopRight);
+    }
+
+    [Fact]
+    public void FromDimensionsAndHandle_RightTop_ExtendsNegative()
+    {
+        Box box = Box.FromDimensionsAndHandle(10, 8, HorizontalAlignment.Right, VerticalAlignment.Top, new Point(0, 0));
+
+        TestHelpers.Near(new Point(-10, -8), box.BottomLeft);
+        TestHelpers.Near(new Point(0, 0), box.TopRight);
+    }
+
+    [Fact]
+    public void FromDimensionsAndHandle_AllNineCombos_CorrectCorners()
+    {
+        (HorizontalAlignment h, VerticalAlignment v, double l, double r, double b, double t)[] cases =
+        [
+            (HorizontalAlignment.Left, VerticalAlignment.Bottom, 0, 10, 0, 6),
+            (HorizontalAlignment.Centre, VerticalAlignment.Bottom, -5, 5, 0, 6),
+            (HorizontalAlignment.Right, VerticalAlignment.Bottom, -10, 0, 0, 6),
+            (HorizontalAlignment.Left, VerticalAlignment.Centre, 0, 10, -3, 3),
+            (HorizontalAlignment.Centre, VerticalAlignment.Centre, -5, 5, -3, 3),
+            (HorizontalAlignment.Right, VerticalAlignment.Centre, -10, 0, -3, 3),
+            (HorizontalAlignment.Left, VerticalAlignment.Top, 0, 10, -6, 0),
+            (HorizontalAlignment.Centre, VerticalAlignment.Top, -5, 5, -6, 0),
+            (HorizontalAlignment.Right, VerticalAlignment.Top, -10, 0, -6, 0),
+        ];
+
+        foreach (var (h, v, l, r, b, t) in cases)
+        {
+            Box box = Box.FromDimensionsAndHandle(10, 6, h, v, new Point(7, 9));
+
+            TestHelpers.Near(new Point(l + 7, b + 9), box.BottomLeft);
+            TestHelpers.Near(new Point(r + 7, t + 9), box.TopRight);
+        }
+    }
+
+    [Fact]
+    public void Transform_Identity_RoundTrips()
+    {
+        Box box = new([new Point(1, 2), new Point(5, 9)]);
+
+        Box result = box.Transform(AffineTransform.Identity());
+
+        TestHelpers.Near(box.BottomLeft, result.BottomLeft);
+        TestHelpers.Near(box.TopRight, result.TopRight);
+    }
+
+    [Fact]
+    public void Transform_Translation_ShiftsBothCorners()
+    {
+        Box box = new([new Point(1, 2), new Point(5, 9)]);
+
+        Box result = box.Transform(AffineTransform.Identity().Translate(10, -3));
+
+        TestHelpers.Near(new Point(11, -1), result.BottomLeft);
+        TestHelpers.Near(new Point(15, 6), result.TopRight);
+        TestHelpers.Near(4, result.Width);
+        TestHelpers.Near(7, result.Height);
+    }
+
+    [Fact]
+    public void Transform_PositiveScaleAroundOrigin_ScalesExtents()
+    {
+        Box box = new([new Point(1, 2), new Point(3, 4)]);
+
+        Box result = box.Transform(AffineTransform.Identity().Scale(2, 3));
+
+        TestHelpers.Near(new Point(2, 6), result.BottomLeft);
+        TestHelpers.Near(new Point(6, 12), result.TopRight);
+    }
+
+    [Fact]
+    public void Transform_MapsOnlyTwoCorners_DocumentsBehavior()
+    {
+        // Box.Transform maps BottomLeft/TopRight only (no 4-corner rotation handling).
+        // Rotation-aware consumers must use GraphicShape.BoundingBox() instead.
+        Box box = new([new Point(0, 0), new Point(10, 4)]);
+
+        Box result = box.Transform(AffineTransform.Identity().Rotate(90));
+
+        // BL(0,0)->(0,0); TR(10,4)->(-4,10); Box of those two alone:
+        TestHelpers.Near(new Point(-4, 0), result.BottomLeft, 1e-9);
+        TestHelpers.Near(new Point(0, 10), result.TopRight, 1e-9);
+    }
+
+    [Fact]
+    public void Transform_YFlip_SwapsVerticalOrder()
+    {
+        Box box = new([new Point(1, 2), new Point(5, 9)]);
+
+        Box result = box.Transform(AffineTransform.Identity().Scale(1, -1));
+
+        // BL(1,2)->(1,-2); TR(5,9)->(5,-9); re-normalized:
+        TestHelpers.Near(new Point(1, -9), result.BottomLeft);
+        TestHelpers.Near(new Point(5, -2), result.TopRight);
+        TestHelpers.Near(4, result.Width);
+        TestHelpers.Near(7, result.Height);
+    }
+
+    [Fact]
+    public void Transform_ZeroSizeBox_StaysZeroSizeUnderTranslation()
+    {
+        Box box = new([new Point(5, 5)]);
+
+        Box result = box.Transform(AffineTransform.Identity().Translate(3, 3));
+
+        TestHelpers.Near(new Point(8, 8), result.BottomLeft);
+        TestHelpers.Near(new Point(8, 8), result.TopRight);
+        TestHelpers.Near(0, result.Width);
+        TestHelpers.Near(0, result.Height);
+    }
+}
